7a4f49cd719f725e94ced9d05dcaaddae69958e1
Daniele Petti <75277685+LilPessy@users.noreply.github.com>
2026-07-01 20:18:15 +0200

    corretta la virgola

--- backend/models/user.js
+++ backend/models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        trim:true
+        trim:true,
         lowercase: true
     },
     password: {

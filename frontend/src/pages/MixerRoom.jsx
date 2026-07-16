0bcca8d3e8a3b8e18028c2ccb398838e48a0db56
Daniele Petti <75277685+LilPessy@users.noreply.github.com>
2026-07-16 13:24:35 +0200

    Aggiornato Mixer

--- frontend/src/pages/MixerRoom.jsx
+++ frontend/src/pages/MixerRoom.jsx
@@ -1,5 +1,37 @@
+import './MixerRoom.css'
+import Button from '../components/Button'
+import Knob from '../components/Knob'
+import ExitIcon from '../assets/exit.png'
+
 function MixerRoom(){
-    return <>Mixer</>
+
+    const handelClick = ()=>{
+        window.location.href = "http://localhost:5173/"
+    }
+
+    return(
+        <section>
+            <div className='header-curvo'></div>
+            <div className='header'>
+                <div className='title'>
+                    <h1>
+                        Amore amaro
+                    </h1>
+                    <p>di Matteo Maria Romano</p>
+                </div>
+                <div className='btnWrap'>
+                    <Button icon={ExitIcon} callback={handelClick}/>
+                </div>
+            </div>
+
+            <div className='knobsContainer'>
+                <Knob freq="HF"/>
+                <Knob freq="MF"/>
+                <Knob freq="LF"/>
+            </div>
+
+        </section>
+    )
 }
 
 export default MixerRoom

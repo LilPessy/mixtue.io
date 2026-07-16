d68c54cd40c3db0484acea8201ac2bea6d3c5a88
Matteo Maria Romano <romano.matteo1f@gmail.com>
2026-07-16 16:38:11 +0200

    added roomCode in sessionSchema

--- backend/models/Session.js
+++ backend/models/Session.js
@@ -15,6 +15,14 @@ const trackSchema = new mongoose.Schema({
 });
 
 const sessionSchema = new mongoose.Schema({
+
+    roomeCode: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        default: () => Math.floor(10000 + Math.random() * 90000).toString()
+    },
     name: {
         type: String,
         required: true,

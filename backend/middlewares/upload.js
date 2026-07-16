8f3ed2df5178553f2cb8ce98baa2b9bac5f7129f
Arianna Tigre <arianna.tigre04@gmail.com>
2026-07-16 15:31:20 +0200

    implementazione multer
    Per poter gestire l'inserimento delle immagini durante la registrazione è necessario utilizzare Multer

---
+++ backend/middlewares/upload.js
@@ -0,0 +1,10 @@
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/propic/'); 
+},
+filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
